6c341e6b6f321914f78f206a3299d6718a3dd066
arshadvkb <arshadvkb118@gmail.com>
2025-10-29 20:48:48 +0530

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home_page from "./pages/Home_page"
+import Signup_page from "./pages/Signup_page";
+import Login_page from "./pages/Login_page";
+import Profile_page from "./pages/Profile_page";
 
 function App() {
  
   return (
     <>
-   
+   <Navbar/>
+   <Routes>
+    <Route path="/" element={<Home_page/>}/>
+    <Route path="/signup" element={<Signup_page/>}/>
+    <Route path="/login" element={<Login_page/>}/>
+    <Route path="/profile" element={<Profile_page/>}/>
+   </Routes>
     </>
   );
 }
