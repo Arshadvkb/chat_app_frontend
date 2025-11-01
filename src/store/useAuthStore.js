a2a8ace84e55ce4f515304ce571ebfc4b0744a22
arshadvkb <arshadvkb118@gmail.com>
2025-11-01 19:42:48 +0530

    added chatting

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -1,11 +1,13 @@
 import { create } from "zustand";
 import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
+import { io } from "socket.io-client";
 
 export const useAuthStore = create((set) => ({
   authUser: null,
   isSigningUp: false,
   isLoggingIn: false,
+  onlineUsers: [],
   isUpdatingProfile: false,
   isCheckingAuth: true,
 
@@ -68,5 +70,26 @@ export const useAuthStore = create((set) => ({
     } finally {
       set({ isUpdatingProfile: false });
     }
+  },
+
+  connectSocket: () => {
+    const { authUser } = get();
+    if (!authUser || get().socket?.connected) return;
+
+    const socket = io(BASE_URL, {
+      query: {
+        userId: authUser._id,
+      },
+    });
+    socket.connect();
+
+    set({ socket: socket });
+
+    socket.on("getOnlineUsers", (userIds) => {
+      set({ onlineUsers: userIds });
+    });
+  },
+  disconnectSocket: () => {
+    if (get().socket?.connected) get().socket.disconnect();
   },
 }));
